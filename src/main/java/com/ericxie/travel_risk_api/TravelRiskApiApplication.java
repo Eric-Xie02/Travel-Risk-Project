282d62b0b6ec21863c3a2951f950cbfe4b478c6d
Eric Xie <exie102202@gmail.com>
2026-03-11 15:55:19 -0400

    Initial commit

---
+++ src/main/java/com/ericxie/travel_risk_api/TravelRiskApiApplication.java
@@ -0,0 +1,13 @@
+package com.ericxie.travel_risk_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelRiskApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelRiskApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ericxie/travel_risk_api/TravelRiskApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ericxie.travel_risk_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelRiskApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
